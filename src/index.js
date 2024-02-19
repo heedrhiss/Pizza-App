a9a95468c1c38271ebb3c64a0ec3a1c6cd7627b9
Heedrhiss <muyideenidris77@gmail.com>
2024-02-19 16:50:37 +0100

    Update-2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
-function App() {
-  return <h1>React from Heedrhi$$</h1>;
-}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Menu />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header container">
+      <h1>Heedrhiss Home of Pizzas</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>OUR MENU</h2>
+    </main>
+  );
+}
